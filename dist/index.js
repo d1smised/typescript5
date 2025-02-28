c63398b6bc6e2d244b5cd0f37726a11d01ba4b53
d1smised <makzar05@gmail.com>
2025-02-28 18:25:32 +0400

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+const data = [];
+const record = (param) => {
+    data.push(param);
+    console.log('Record: ' + param + ' added!');
+    setTimeout(() => {
+        const index = data.indexOf(param);
+        if (index > -1) {
+            data.splice(index, 1); // Удаляем элемент из массива
+            console.log('Record: ' + param + ' removed!');
+        }
+    }, 10000);
+};
+const createLogger = (message) => {
+    return () => {
+        console.log(message);
+    };
+};
+const logHello = createLogger('Hello, World!');
+const logGoodbye = createLogger('Goodbye, World!');
+logHello();
+logGoodbye();
+record('Test Record 1');
+record('Test Record 2');
